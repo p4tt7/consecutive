443541a8e73a72860735195c0bbe14453f57669a
patty 💜 <87675545+p4tt7@users.noreply.github.com>
2024-12-29 17:58:54 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class Program{
+    public static bool Consecutive(int [] a, int k){
+
+        int contador = 1;
+        for(int i=0;i<a.Length-1;i++){
+            if(a[i]==a[i+1]){
+                contador++;
+            }
+            else{
+                if(contador==k){
+                    return true;
+                }
+                contador=1; 
+            }
+        }  
+        if(contador==k){
+            return true;
+        }
+        return false;  
+    }
+
+    public static void Main(){
+        int[] a = {1,1,1,1,1};
+        int k=5;
+        bool solution = Consecutive(a, k);
+        Console.WriteLine(solution);
+    }
+}
